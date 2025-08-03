750f2e91a49e3d509e55f78c13fc03e40a865822
John Anthony Rico <johnrico364@gmail.com>
2025-08-03 23:58:22 +0800

    add initial product schema design

--- server/models/productsSchema.js
+++ server/models/productsSchema.js
@@ -4,8 +4,11 @@ const Schema = mongoose.Schema;
 const productSchema = new Schema(
   {
     name: { type: String, required: true },
-    modelNumber: { type: String, required: true },
     barcode: { type: Number, required: true, unique: true },
+    modelNumber: { type: String, required: true },
+    sizeLabel: {type: String, required: true },
+    category: { type: String, required: true },
+    description: { type: String, required: true },
     stocks: { type: Number, required: true },
     isDeleted: { type: Boolean, required: true, default: false },
   },
