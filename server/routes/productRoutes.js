ff128e99bbac751f2ae3bffbdc2a59471c7983b9
johnrico364 <johnrico364@gmail.com>
2025-08-04 13:22:20 +0800

    Add update, get all, and soft delete

--- server/routes/productRoutes.js
+++ server/routes/productRoutes.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const Controller = require('../controllers/productController');
+const express = require("express");
+const Controller = require("../controllers/productController");
 
 const router = express.Router();
 
-router.post('/add', Controller.addProduct);
+router.post("/add", Controller.addProduct);
+router.get("/get", Controller.getProducts);
+router.patch("/update/:id", Controller.updateProduct);
+router.patch("/delete/:id", Controller.deleteProduct);
 
 module.exports = router;
