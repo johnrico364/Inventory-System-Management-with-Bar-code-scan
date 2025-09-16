6a02d7cec5a88018405f2161868cee156d196558
John Anthony Rico <johnrico364@gmail.com>
2025-09-17 00:29:04 +0800

    Add the initial routes, controllers, and schema for USER

--- server/index.js
+++ server/index.js
@@ -3,27 +3,26 @@ const mongoose = require("mongoose");
 const path = require("path");
 const cors = require("cors");
 
-// routes imports
-const productsRoutes = require("./routes/productRoutes");
-const transactionRoutes = require("./routes/transactionRoutes");
-
 const app = express();
 
 // CORS configuration
-app.use(cors({
-  // origin: 'http://localhost:3000',
-  origin: 'https://mom-inventory-system.vercel.app',
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-  credentials: true
-}));
+app.use(
+  cors({
+    // origin: 'http://localhost:3000',
+    origin: "https://mom-inventory-system.vercel.app",
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 
 // Serve static files from barcodes directory
-app.use('/barcodes', express.static(path.join(__dirname, 'barcodes')));
+app.use("/barcodes", express.static(path.join(__dirname, "barcodes")));
 
-const dbURI = 'mongodb+srv://john:John2004@personalproject.fkzdsfo.mongodb.net/inventory_system?retryWrites=true&w=majority&appName=PersonalProject'
+const dbURI =
+  "mongodb+srv://john:John2004@personalproject.fkzdsfo.mongodb.net/inventory_system?retryWrites=true&w=majority&appName=PersonalProject";
 
 mongoose.connect(dbURI).then(() => {
   console.log("Connected to MongoDB");
@@ -31,5 +30,11 @@ mongoose.connect(dbURI).then(() => {
 
 app.listen(4000, () => console.log(`Listening on port 4000`));
 
+// routes imports
+const productsRoutes = require("./routes/productRoutes");
+const transactionRoutes = require("./routes/transactionRoutes");
+const userRoutes = require("./routes/userRoutes");
+
 app.use("/api/products", productsRoutes);
-app.use('/api/transactions', transactionRoutes);
+app.use("/api/transactions", transactionRoutes);
+app.use("/api/user", userRoutes);
---
+++ server/routes/userRoutes.js
@@ -0,0 +1,5 @@
+const express = require("express");
+const Controller = require("../controllers/userController");
+
+const router = express.Router();
+
