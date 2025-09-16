64c107a06e390ba3cd1315825c06ac6b55c54441
Ja Rico <johnrico364@gmail.com>
2025-09-16 11:35:41 +0800

    remove the slash

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ const app = express();
 // CORS configuration
 app.use(cors({
   // origin: 'http://localhost:3000',
-  origin: 'https://mom-inventory-system.vercel.app/',
+  origin: 'https://mom-inventory-system.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
